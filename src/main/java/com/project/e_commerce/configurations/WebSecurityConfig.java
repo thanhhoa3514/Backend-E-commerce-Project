974dc28a67b1a3684562c3ed7d00cbd90986e72b
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-03-11 20:55:51 +0700

    Configuration web security with permit all request but soonly filter request

--- src/main/java/com/project/e_commerce/configurations/WebSecurityConfig.java
+++ src/main/java/com/project/e_commerce/configurations/WebSecurityConfig.java
@@ -1,4 +1,27 @@
 package com.project.e_commerce.configurations;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests->{
+                    requests.requestMatchers("**").permitAll();
+                })
+                .build();
+    }
 }
