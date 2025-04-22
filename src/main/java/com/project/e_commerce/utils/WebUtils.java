28de8b2315720aa3247a2b2ccde108ff8ad81a00
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-04-22 16:28:01 +0700

    create webutils to get currently request

---
+++ src/main/java/com/project/e_commerce/utils/WebUtils.java
@@ -0,0 +1,20 @@
+package com.project.e_commerce.utils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class WebUtils {
+    public static HttpServletRequest getCurrentRequest() {
+        try {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                return attributes.getRequest();
+            }
+            return null;
+        } catch (Exception e) {
+            // Log error if needed
+            return null;
+        }
+    }
+}
