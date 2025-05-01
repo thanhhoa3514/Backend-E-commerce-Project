3aecd0be3540fc33b3316f76ea1838fa2de964ff
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-01 15:49:25 +0700

    Update OrderDetailController.java

--- src/main/java/com/project/e_commerce/controllers/OrderDetailController.java
+++ src/main/java/com/project/e_commerce/controllers/OrderDetailController.java
@@ -1,11 +1,19 @@
 package com.project.e_commerce.controllers;
 
+import com.project.e_commerce.components.LocalizationUtils;
 import com.project.e_commerce.dtos.order.OrderDetailDTO;
+import com.project.e_commerce.responses.ApiResponse;
 import com.project.e_commerce.responses.OrderDetailResponse;
+import com.project.e_commerce.responses.ResponseObject;
 import com.project.e_commerce.services.orderdetails.OrderDetailService;
+import com.project.e_commerce.utils.MessageKeys;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +24,20 @@ import java.util.List;
 public class OrderDetailController {
 
     private final OrderDetailService orderDetailService;
+    private final LocalizationUtils localizationUtils;
 
     @PostMapping()
-    public ResponseEntity<?> createOrderDetail(@Valid @RequestBody OrderDetailDTO orderDetailDTO) {
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    public ResponseEntity<ApiResponse> createOrderDetail(@Valid @RequestBody OrderDetailDTO orderDetailDTO) {
         try {
             OrderDetailResponse orderDetail = orderDetailService.createOrderDetail(orderDetailDTO);
-            return ResponseEntity.ok().body(orderDetail);
+            return ResponseEntity.ok().body(
+                    ApiResponse.builder().message("Create order detail successfully").status(HttpStatus.CREATED.value()).data(orderDetail).build()
+            );
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().body(
+                    ApiResponse.builder().message("Create order detail successfully").status(HttpStatus.CREATED.value()).data(null).build()
+            );
         }
     }
 
@@ -31,7 +45,14 @@ public class OrderDetailController {
     public ResponseEntity<?> getOrderDetail(@Valid @PathVariable("id") Long id) {
         try {
             OrderDetailResponse orderDetail = orderDetailService.getOrderDetailById(id);
-            return ResponseEntity.ok().body(orderDetail);
+            return ResponseEntity.ok().body(
+                    ApiResponse.builder()
+                            .message("Get order detail successfully")
+                            .status(HttpStatus.OK.value())
+                            .data(orderDetail)
+                            .build()
+
+            );
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -41,29 +62,52 @@ public class OrderDetailController {
     public ResponseEntity<?> getOrderDetails(@PathVariable("orderId") Long orderId) {
         try {
             List<OrderDetailResponse> orderDetails = orderDetailService.getAllOrderDetailsByOrderId(orderId);
-            return ResponseEntity.ok().body(orderDetails);
+            return ResponseEntity.ok().body(
+                    ApiResponse.builder()
+                            .message("Get order details by orderId successfully")
+                            .status(HttpStatus.OK.value())
+                            .data(orderDetails)
+                            .build()
+
+            );
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @PutMapping("/{id}")
+
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public ResponseEntity<?> updateOrderDetail(
             @Valid @RequestBody OrderDetailDTO orderDetailDTO,
             @PathVariable("id") Long id) {
         try {
             OrderDetailResponse updatedOrderDetail = orderDetailService.updateOrderDetail(id, orderDetailDTO);
-            return ResponseEntity.ok().body(updatedOrderDetail);
+            return ResponseEntity.ok().body(
+                    ResponseObject
+                            .builder()
+                            .data(updatedOrderDetail)
+                            .message("Update order detail successfully")
+                            .status(HttpStatus.OK)
+                            .build());
+
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @DeleteMapping("/{id}")
+    @Operation(security = { @SecurityRequirement(name = "bearer-key") })
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public ResponseEntity<?> deleteOrderDetail(@PathVariable("id") Long id) {
         try {
             orderDetailService.deleteOrderDetail(id);
-            return ResponseEntity.ok().body("Order detail deleted successfully");
+            return ResponseEntity.ok().body(
+                    ResponseObject.builder()
+                    .message(localizationUtils
+                            .getLocalizedMessage(MessageKeys.DELETE_ORDER_DETAIL_SUCCESSFULLY))
+                    .build());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
