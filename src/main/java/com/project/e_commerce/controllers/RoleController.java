e67f7c25b289a2010b9d4639bf951e6250f3e814
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-02 12:13:59 +0700

    Update RoleController.java

--- src/main/java/com/project/e_commerce/controllers/RoleController.java
+++ src/main/java/com/project/e_commerce/controllers/RoleController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("${api.prefix}/roles")
+@RequestMapping("${api.prefix}/admin/roles")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class RoleController {
