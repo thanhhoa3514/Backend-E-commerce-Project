fa4d4582b6f3191e7feac8d48ceae3557caf95d6
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-03 14:52:52 +0700

    update: configurage multilanguage

--- src/main/java/com/project/e_commerce/configurations/LanguageConfig.java
+++ src/main/java/com/project/e_commerce/configurations/LanguageConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
-public class LanguageConfig {
+public class LanguageConfig implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource(){
@@ -15,5 +22,24 @@ public class LanguageConfig {
         messageSource.setBasename("i18n:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+    
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US); // Mặc định là tiếng Anh
+        return localeResolver;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang"); // Tham số để thay đổi ngôn ngữ
+        return interceptor;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
