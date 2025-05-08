5e87faf64770b644e9e6d48d5c327bafe9e7cd88
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-08 19:43:11 +0700

    fix : add constructor and no constructure for user register dto

--- src/main/java/com/project/e_commerce/dtos/user/UserRegisterDTO.java
+++ src/main/java/com/project/e_commerce/dtos/user/UserRegisterDTO.java
@@ -6,11 +6,15 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRegisterDTO {
     @JsonProperty("fullname")
     @NotBlank(message = "Full name is required")
