aa3cb7cd51c491b7dd45dda7d84805d697e9ec59
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-09 16:22:07 +0700

    make : change otp vetify requesting from client

--- src/main/java/com/project/e_commerce/dtos/otp/OTPVerifyRequestDTO.java
+++ src/main/java/com/project/e_commerce/dtos/otp/OTPVerifyRequestDTO.java
@@ -2,7 +2,6 @@ package com.project.e_commerce.dtos.otp;
 
 
 import com.project.e_commerce.enums.OTPType;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,10 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OTPVerifyRequestDTO {
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email must be valid")
-    private String email;
-
     @NotBlank(message = "OTP code is required")
     private String code;
 
