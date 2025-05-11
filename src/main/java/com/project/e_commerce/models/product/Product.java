c01f3f3e5c5ddc4cee5f85137d928e19aec9ef87
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2025-05-11 16:16:52 +0700

    fix : ignore json ignore for reducing data caching in redis

--- src/main/java/com/project/e_commerce/models/product/Product.java
+++ src/main/java/com/project/e_commerce/models/product/Product.java
@@ -7,9 +7,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.List;
-
 
 @Entity
 @Getter
@@ -18,37 +18,31 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Table(name = "products")
-//Event-driven approach with Spring Data JPA
-//@EntityListeners(ProductListener.class)
+// Event-driven approach with Spring Data JPA
+// @EntityListeners(ProductListener.class)
 public class Product extends BaseEntity {
-    @Column(name = "name", nullable = false,length = 350)
+    @Column(name = "name", nullable = false, length = 350)
     private String name;
 
     @Column(name = "price", nullable = false)
     private double price;
 
-
     @Column(name = "thumbnail")
     private String thumbnail;
 
-
-    @Column(name = "quantity",nullable = false)
+    @Column(name = "quantity", nullable = false)
     @Min(0)
     private int quantity;
 
-    @Column(name="description")
+    @Column(name = "description")
     private String description;
-
-
 
     @NotNull(message = "Category ID must not be null")
     @ManyToOne
-    @JoinColumn(name="category_id")
+    @JoinColumn(name = "category_id")
     private Category categoryId;
 
-
-    @OneToMany(mappedBy = "product",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<ProductImage> productImages;
 }
